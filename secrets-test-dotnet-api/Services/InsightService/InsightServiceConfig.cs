2c8f4179cb3b4af6b4455c8c2ed3756e791fd1a2
Terraform User <brett.lewerke@insight.com>
2025-09-18 11:52:25 -0700

    Insight GitHub Repository Template

---
+++ secrets-test-dotnet-api/Services/InsightService/InsightServiceConfig.cs
@@ -0,0 +1,7 @@
+namespace secrets_test_dotnet_api.Services;
+
+public class InsightServiceConfig {
+    public const string Config = "InsightService";
+
+    public string? ConfigValue { get; set;}
+}
